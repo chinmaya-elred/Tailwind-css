48350ad4e9d9e794a087bb8546c713d97094151e
chinmaya-elred <chinmaya@elred.io>
2024-07-15 13:44:04 +0530

    completed tailwind css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-// import { close, logo, menu } from "../assets";
+import { close, logo, menu } from "../assets";
 import { navLinks } from "../constants";
 
 const Navbar = () => {
@@ -9,7 +9,7 @@ const Navbar = () => {
 
   return (
     <nav className="w-full flex py-6 justify-between items-center navbar">
-      {/* <img src={logo} alt="hoobank" className="w-[124px] h-[32px]" /> */}
+      <img src={logo} alt="hoobank" className="w-[124px] h-[32px]" />
 
       <ul className="list-none sm:flex hidden justify-end items-center flex-1">
         {navLinks.map((nav, index) => (
@@ -27,7 +27,7 @@ const Navbar = () => {
 
       <div className="sm:hidden flex flex-1 justify-end items-center">
         <img
-          // src={toggle ? close : menu}
+          src={toggle ? close : menu}
           alt="menu"
           className="w-[28px] h-[28px] object-contain"
           onClick={() => setToggle(!toggle)}
--- src/components/index.js
+++ src/components/index.js
@@ -1,29 +1,23 @@
-import Billing from "./Billing";
-import Business from "./Business";
-import Button from "./Button";
-import CardDeal from "./CardDeal";
-import Client from "./Client";
-import CTA from "./CTA";
-import FeedbackCard from "./FeedbackCard";
-import Footer from "./Footer";
-import GetStarted from "./GetStarted";
-import Hero from "./Hero";
 import Navbar from "./Navbar";
+import Billing from "./Billing";
+import CardDeal from "./CardDeal";
+import Business from "./Business";
+import Clients from "./Clients";
+import CTA from "./CTA";
 import Stats from "./Stats";
+import Footer from "./Footer";
 import Testimonials from "./Testimonials";
+import Hero from "./Hero";
 
 export {
-    Billing,
-    Business,
-    Button,
-    CardDeal,
-    Client,
-    CTA,
-    FeedbackCard,
-    Footer,
-    GetStarted,
-    Hero,
-    Navbar,
-    Stats,
-    Testimonials
-}
+  Navbar,
+  Billing,
+  CardDeal,
+  Business,
+  Clients,
+  CTA,
+  Stats,
+  Footer,
+  Testimonials,
+  Hero,
+};
